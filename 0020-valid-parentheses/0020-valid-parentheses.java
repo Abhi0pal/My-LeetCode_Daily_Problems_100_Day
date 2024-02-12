e03282e14dae12fd54f95a4232eb261ab0d1af05
Abhi0pal <106912617+Abhi0pal@users.noreply.github.com>
2024-02-13 00:58:21 +0530

    Update 0020-valid-parentheses.java
    //new approach of this code

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,34 +2,54 @@ import java.util.*;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> sb=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            //If opening baces, stack push;
-            //if closing baces, Stack pop;
-            if(ch=='(' ||ch=='{'||ch=='['){
-                sb.push(ch);
-            }
-            else{
-                if(!sb.empty()){
-                    char top=sb.peek();
-                    if(ch==')' &&top=='('||
-                    ch==']' &&top=='['||
-                    ch=='}' &&top=='{'){
-                        sb.pop();
-                    }
-                    else{
-                        return false;
-                    }
-                }
-                else{
-                    return false;
-                }
-            } 
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     //If opening baces, stack push;
+        //     //if closing baces, Stack pop;
+        //     if(ch=='(' ||ch=='{'||ch=='['){
+        //         sb.push(ch);
+        //     }
+        //     else{
+        //         if(!sb.empty()){
+        //             char top=sb.peek();
+        //             if(ch==')' &&top=='('||
+        //             ch==']' &&top=='['||
+        //             ch=='}' &&top=='{'){
+        //                 sb.pop();
+        //             }
+        //             else{
+        //                 return false;
+        //             }
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     } 
+        // }
+        // if(sb.empty()){
+        //     return true;
+        // }
+         // return false;
+
+        
+         // Here is the new code or we can say new approach of this code 
+
+        //here is the use new method 
+    for(char c:s.toCharArray()){
+        if(c=='('){
+            sb.push(')');
         }
-        if(sb.empty()){
-            return true;
+        else if(c=='{'){
+            sb.push('}');
         }
-        return false;
+        else if(c=='['){
+            sb.push(']');
+        }
+        else if(sb.isEmpty() || sb.pop()!=c){
+            return false;
+        }
+    }
+    return sb.isEmpty(); 
  
     }
         
