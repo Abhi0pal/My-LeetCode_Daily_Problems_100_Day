4452cafbd132b388f19117c7dd409175090e58d3
Abhi0pal <106912617+Abhi0pal@users.noreply.github.com>
2024-04-13 23:41:02 +0530

    Time: 4 ms (36.51%), Space: 8.8 MB (9.69%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,23 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//approach first
+
+//seacond approach is slow or fast
+
+int length(ListNode* head){
+    int count=0;
+    ListNode* temp=head;
+    while(temp!=NULL){
+        temp=temp->next;
+        count++;
+    }
+    return count;
+
+}
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if(head==NULL&&head->next==NULL){
-            return head; 
-        }
+        // int len=length(head)/2;
+        // ListNode* temp=head;
+        // while(len--){
+        //     temp=temp->next;
+
+        // }
+        // return temp;
+
+
+        //approch two
+        // if(head==NULL ||head->next==NULL){
+        //     return head;
+
+        // }
         ListNode* slow=head;
         ListNode* fast=head->next;
         while(fast!=NULL){
             slow=slow->next;
-           fast=fast->next;
-           if(fast!=NULL){
             fast=fast->next;
-           }
-
+            if(fast!=NULL){
+                fast=fast->next;
+            }
+           
         }
         return slow;
+
+
         
     }
 };
