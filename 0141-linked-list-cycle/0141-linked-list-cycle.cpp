769325135287868b09185c7a6670b9a45accb06c
Abhi0pal <106912617+Abhi0pal@users.noreply.github.com>
2024-04-14 02:02:42 +0530

    Time: 24 ms (6.00%), Space: 13.7 MB (8.15%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,22 +9,36 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL){
+        if(head==NULL|| head->next==NULL){
             return false;
         }
         //FLOYD DETECT CYCLE
-        ListNode*slow=head;
-        ListNode*fast=head;
-        while(fast!=NULL){
-            fast=fast->next;
-            if(fast!=NULL){
-                fast=fast->next;
-                slow=slow->next;
-            }
-            if(slow==fast){
+        // ListNode*slow=head;
+        // ListNode*fast=head;
+        // while(fast!=NULL){
+        //     fast=fast->next;
+        //     if(fast!=NULL){
+        //         fast=fast->next;
+        //         slow=slow->next;
+        //     }
+        //     if(slow==fast){
+        //         return true;
+        //     }
+        // }
+        // return false;
+         
+        //Through Map
+        map<ListNode*,bool>mpp;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            if(mpp[temp]==true){
                 return true;
             }
+            mpp[temp]=true;
+            temp=temp->next;
         }
         return false;
+
+
     }
 }; 
