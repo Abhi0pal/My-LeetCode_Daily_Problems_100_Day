28eb368dc2b58fd0bcfec9a8165b5c05fdf7c89a
Abhishek Pal <106912617+Abhi0pal@users.noreply.github.com>
2024-08-09 14:44:25 +0530

    Time: 0 ms (100%), Space: 41.8 MB (62.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,50 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(target==nums[i]){
-                return i;
+    int binary_search(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
             }
         }
         return -1;
-        
+    }
+
+    int findPivot(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            // Correctly check the mid conditions
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && nums[mid - 1] > nums[mid]) {
+                return mid - 1;
+            }
+            if (nums[start] >= nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        if (pivot == -1) {
+            return binary_search(nums, target, 0, nums.length - 1);
+        }
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+        if (target >= nums[0]) {
+            return binary_search(nums, target, 0, pivot - 1);
+        }
+        return binary_search(nums, target, pivot + 1, nums.length - 1);
     }
 }
