b3b6c1741d3a58a88dd9535d52226b9b62724d36
Abhishek Pal <106912617+Abhi0pal@users.noreply.github.com>
2024-08-16 16:10:42 +0530

    Time: 2 ms (87.36%), Space: 41.3 MB (63.87%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -3,18 +3,20 @@ class Solution {
         if(n==1){
             return "1";
         }
-        // String result="";
         StringBuilder res=new StringBuilder();
-        String say=countAndSay(n-1);
-        int count=0;
-        for(int i=0;i<say.length();i++){
-           count++;
-           if(i==say.length()-1||say.charAt(i)!=say.charAt(i+1)){
-               //Becoz of this this our code or line our runtime comlexity is--> (17ms)
-              // result=result+count+say.charAt(i); //result=count+element
-              res.append(count).append(say.charAt(i));
-               count=0;
-           }
+        String str=countAndSay(n-1);
+        int count=1;
+
+        for(int i=0;i<str.length();i++){
+            // String str1=str.charAt(i);
+            if(i==str.length()-1||str.charAt(i)!=str.charAt(i+1)){
+                res.append(count).append(str.charAt(i));
+                count=1;
+            }
+            else{
+                count++;
+                
+            }
         }
         return res.toString();
     }
